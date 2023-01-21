c636a41085b05efc9fa3f6522e08485d3d6fb8b5
bryanmtxd <67843182+bchh325@users.noreply.github.com>
2023-01-21 02:54:12 -0800

    prototype skeleton

---
+++ src/components/ContentContainer.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './ContentContainer.module.css'
+
+export default function ContentContainer() {
+  return (
+    <div className={styles.content}>ContentContainer</div>
+  )
+}
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './Navigation.module.css'
+
+export default function Navigation() {
+  return (
+    <div className={styles.nav}>Navigation</div>
+  )
+}
