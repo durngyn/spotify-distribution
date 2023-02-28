7756f880c82169d841ea90abb419492f41ecf92c
durngyn <darrennguyen321919@gmail.com>
2023-02-28 04:57:41 -0800

    login btn

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import styles from './Login.module.css'
+import { FaSpotify } from "react-icons/fa";
 
 export default function Login() {
   return (
     <div className={styles.container}>
             <h1 className={styles.header}>
-                <span>Graphify wants to login to Spotify</span>
+                <p className={styles.login}>
+                </p>
                 {/* <span className={styles.shadow}>Login</span>   */}
-                <button>Login</button>
+                <button> 
+                    <FaSpotify className={styles.logo}size={90} />
+                    <span className={styles.word}>Login to Spotify</span>
+                </button>
             </h1>
     </div>
   )
