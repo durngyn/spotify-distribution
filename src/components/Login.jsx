7b73b0165ff8f6210c6ecf579f67551a1332429b
durngyn <darrennguyen321919@gmail.com>
2023-03-01 01:11:38 -0800

    a lot

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import styles from './Login.module.css'
 import { FaSpotify } from "react-icons/fa";
+import { HiArrowsUpDown } from "react-icons/hi2";
+import gr from '../images/gr.png'
 
 export default function Login() {
   return (
     <div className={styles.container}>
-            <h1 className={styles.header}>
-                <p className={styles.login}>
-                </p>
-                {/* <span className={styles.shadow}>Login</span>   */}
+                <div className={styles.login}>
+                  <img className={styles.pic} src={gr}/>
+                  <HiArrowsUpDown className={styles.pic2}size={80} />
+                  <FaSpotify className={styles.pic3}size={90} />
+                </div>  
+                <h1 className={styles.header}>
                 <button> 
                     <FaSpotify className={styles.logo}size={90} />
                     <span className={styles.word}>Login to Spotify</span>
