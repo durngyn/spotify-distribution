d366d14e6b1f647af242f0cfb74c106694dffeba
bryanmtxd <67843182+bchh325@users.noreply.github.com>
2023-03-03 04:33:57 -0800

    graph hover styling

--- src/App.js
+++ src/App.js
@@ -6,20 +6,42 @@ import Navigation from './components/Navigation';
 import NullPage from './components/NullPage';
 import PrivacyPage from './components/PrivacyPage';
 import Login from './components/Login';
+import { useRef, useState, useEffect } from 'react';
 
 function App() {
+  const [hover, setHover] = useState("")
+  const backgroundRef = useRef(null)
+
+  const handleMouseEnter = (id) => {
+    console.log("mouseEnter")
+    setHover("background-hover")
+  }
+
+  const handleMouseExit = (id) => {
+    setHover("")
+    const currentRef = backgroundRef.current
+  }
+
+  const handleBar = (id) => {
+    const currentRef = backgroundRef.current
+    currentRef.dataset.active = id
+    console.log(currentRef.dataset.active)
+    console.log(backgroundRef)
+  }
+
   return (
-    <div className="App">
+    <div id="background" className="App">
       <Navigation />
       <div className="content-container">
         <Routes>
-          <Route path="/" element={<GraphPage />} />
+          <Route path="/" element={<GraphPage handleMouseEnter={handleMouseEnter} handleMouseExit={handleMouseExit} handleBar={handleBar}/>} />
           <Route path="spotify-distribution/privacy-policy" element={<PrivacyPage />} />
           <Route path="spotify-distribution/callback" element={<CallbackPage />} />
           <Route path="/*" element={<NullPage />} />
           <Route path="/login" element={<Login />} />
         </Routes>
       </div>
+      <div ref={backgroundRef} id="background-img" className={`background ${hover}`}></div>
     </div>
   );
 }
--- src/components/GraphPage.jsx
+++ src/components/GraphPage.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import CountUp from 'react-countup';import styles from './GraphPage.module.css'
 
-export default function GraphPage() {
+export default function GraphPage({ handleMouseEnter, handleMouseExit, handleBar }) {
     const x = ["A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D"]
+    const graphRef = useRef(null)
 
     for (var i; i < 50; i++) {
         x.push("")
     }
 
-    console.log(x)
     return (
         <div className={styles["page-container"]}>
             <div className={styles["playlist-container"]}>
@@ -24,11 +24,11 @@ export default function GraphPage() {
                         <span>Genre</span>
                         <span>Genre</span>
                     </div>
-                    <div className={styles.bars}>
-                        <div className={styles.short}><CountUp duration={1} end={25} />%</div>
-                        <div className={styles.medium}><CountUp duration={1.5} end={50} />%</div>
-                        <div className={styles.long}>Not currently using actual data</div>
-                        <div className={styles.short}><CountUp duration={1} end={25} />%</div>
+                    <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseExit} id="bar-graph" className={styles.bars}>
+                        <div onMouseEnter={() => handleBar(1)} className={styles.short}><CountUp duration={1} end={25} />%</div>
+                        <div onMouseEnter={() => handleBar(2)} className={styles.medium}><CountUp duration={1.5} end={50} />%</div>
+                        <div onMouseEnter={() => handleBar(3)} className={styles.long}>Not currently using actual data</div>
+                        <div onMouseEnter={() => handleBar(4)} className={styles.short}><CountUp duration={1} end={25} />%</div>
                     </div>
                 </div>
             </div>
