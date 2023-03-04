cf108fcef346939a852245a7a4fdb0d6ec121e55
durngyn <darrennguyen321919@gmail.com>
2023-03-04 04:01:52 -0800

    song graph feature

--- src/components/GraphPage.jsx
+++ src/components/GraphPage.jsx
@@ -1,9 +1,27 @@
 import React, { useRef } from 'react'
-import CountUp from 'react-countup';import styles from './GraphPage.module.css'
+import CountUp from 'react-countup'; import styles from './GraphPage.module.css'
+import Songs from './Songs';
+import { useState, useEffect } from 'react';
 
 export default function GraphPage({ handleMouseEnter, handleMouseExit, handleBar }) {
-    const x = ["A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D"]
+    const x = ["A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D", "A", "B", "C", "D"]
+    const [showSongs, setShowSongs] = useState(false)
     const graphRef = useRef(null)
+    const songRef = useRef(null)
+
+    const handleRef = (ref) => {
+        console.log(ref)
+    }
+
+    const handleClick = () => {
+        setShowSongs(true)
+    }
+
+    const handleCancel = () => {
+        setShowSongs(false)
+    }
+    useEffect(() => {
+    }, [showSongs])
 
     for (var i; i < 50; i++) {
         x.push("")
@@ -16,7 +34,7 @@ export default function GraphPage({ handleMouseEnter, handleMouseExit, handleBar
                     return <span className={styles.playlist}>{item + " playlist item"}</span>
                 })}
             </div>
-            <div className={styles["graph-container"]}>
+            <div className={styles["content-container"]}>
                 <div className={styles.graph}>
                     <div className={styles.genres}>
                         <span>Genre</span>
@@ -25,13 +43,16 @@ export default function GraphPage({ handleMouseEnter, handleMouseExit, handleBar
                         <span>Genre</span>
                     </div>
                     <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseExit} id="bar-graph" className={styles.bars}>
-                        <div onMouseEnter={() => handleBar(1)} className={styles.short}><CountUp duration={1} end={25} />%</div>
-                        <div onMouseEnter={() => handleBar(2)} className={styles.medium}><CountUp duration={1.5} end={50} />%</div>
-                        <div onMouseEnter={() => handleBar(3)} className={styles.long}>Not currently using actual data</div>
-                        <div onMouseEnter={() => handleBar(4)} className={styles.short}><CountUp duration={1} end={25} />%</div>
+                        <div onClick={() => handleClick(1)} onMouseEnter={() => handleBar(1)} className={styles.short}><CountUp duration={1} end={25} />%</div>
+                        <div onClick={() => handleClick(2)} onMouseEnter={() => handleBar(2)} className={styles.medium}><CountUp duration={1.5} end={50} />%</div>
+                        <div onClick={() => handleClick(3)} onMouseEnter={() => handleBar(3)} className={styles.long}>Not currently using actual data</div>
+                        <div onClick={() => handleClick(4)} onMouseEnter={() => handleBar(4)} className={styles.short}><CountUp duration={1} end={25} />%</div>
                     </div>
                 </div>
+                {showSongs && <Songs handleRef={handleRef} handleCancel={handleCancel} className={styles["songs-container"]} />}
             </div>
-        </div>
+
+
+        </div >
     )
 }
