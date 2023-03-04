12f8fa9308a80b80cd09eef83c006989763c413d
bryanmtxd <67843182+bchh325@users.noreply.github.com>
2023-03-04 04:04:00 -0800

    pkce verifier/challenge

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,28 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './Login.module.css'
 import { FaSpotify } from "react-icons/fa";
-import { HiArrowsUpDown } from "react-icons/hi2";
-import { BsArrowLeftRight } from "react-icons/bs";
 import { CgArrowsExchange } from "react-icons/cg";
-// import gr from '../images/gr.png'
 import gr from '../images/grwhite.png'
-// import gr from '../images/groutline.png'
-// import gr from '../images/grb.png'
+import axios from 'axios';
+import authHelpers from './helpers/authorizationHelpers'
 
 export default function Login() {
+  const [url, setUrl] = useState("test")
+  const [authPair, setAuthPair] = useState({
+    verifier: "",
+    challenge: ""
+  })
+
+  const handleLogin = () => {
+    const verifier = authHelpers.generateCodeVerifier()
+
+    authHelpers.createCodeChallenge(verifier)
+      .then(challenge => {
+        setAuthPair({
+          verifier: verifier,
+          challenge: challenge
+        })
+      })
+
+  }
+
+  useEffect(() => {
+    console.log(authPair)
+  }, [authPair])
+
   return (
     <div className={styles.container}>
-                <div className={styles.login}>
-                  <img className={styles.pic} src={gr}/>
-                  <CgArrowsExchange className={styles.pic2}size={90} />
-                  <FaSpotify className={styles.pic3}size={90} />
-                </div>  
-                <h1 className={styles.header}>
-                <button> 
-                    <FaSpotify className={styles.logo}size={90} />
-                    <span className={styles.word}>Login to Spotify</span>
-                </button>
-            </h1>
+      <div className={styles.login}>
+        <img className={styles.pic} src={gr} />
+        <CgArrowsExchange className={styles.pic2} size={90} />
+        <FaSpotify className={styles.pic3} size={90} />
+      </div>
+      <h1 className={styles.header}>
+        <button onClick={handleLogin}>
+          <FaSpotify className={styles.logo} size={90} />
+          <span className={styles.word}>Login to Spotify</span>
+        </button>
+      </h1>
     </div>
   )
 }
---
+++ src/components/helpers/authorizationHelpers.js
@@ -0,0 +1,45 @@
+const authHelpers = {
+    generateCodeVerifier: function () {
+        function dec2hex(dec) {
+            return ('0' + dec.toString(16)).substr(-2)
+        }
+
+        function generateRandomString() {
+            var array = new Uint32Array(56 / 2);
+            window.crypto.getRandomValues(array);
+            return Array.from(array, dec2hex).join('');
+        }
+
+        return generateRandomString();
+    },
+    createCodeChallenge: function (code_verifier) {
+        function sha256(plain) {
+            const encoder = new TextEncoder();
+            const data = encoder.encode(plain);
+            return window.crypto.subtle.digest('SHA-256', data);
+        }
+
+        function base64urlencode(a) {
+            var str = "";
+            var bytes = new Uint8Array(a);
+            var len = bytes.byteLength;
+            for (var i = 0; i < len; i++) {
+                str += String.fromCharCode(bytes[i]);
+            }
+            return btoa(str)
+                .replace(/\+/g, "-")
+                .replace(/\//g, "_")
+                .replace(/=+$/, "");
+        }
+
+        async function challenge_from_verifier(v) {
+            const hashed = await sha256(v);
+            const base64encoded = base64urlencode(hashed);
+            return base64encoded;
+        }
+
+        return challenge_from_verifier(code_verifier);
+    }
+}
+
+export default authHelpers;
